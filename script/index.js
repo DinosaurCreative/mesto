0e85cc09f5d676fa8bdf7a06b8a3fdf92aa091e5
German Gimalaev <gimalaevgerman@yandex.ru>
2021-03-17 18:36:21 +0300

    Переделай js согласно с замечаниями ревьера

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 let page = document.querySelector('.page');
 let popup = page.querySelector('.popup');
-let addButton = page.querySelector('.popup__save-btn');
 let closeButton = page.querySelector('.popup__cross');
 let editButton = page.querySelector('.profile__edit-button');
 let profileName = page.querySelector('.profile__name');
@@ -9,20 +8,23 @@ let inputName = page.querySelector('.popup__input_type_name');
 let inputOccupation = page.querySelector('.popup__input_type_occupation');
 let inputForm = page.querySelector('.popup__input-form');
 
-function showOrHidePopup() {
-  popup.classList.toggle('popup_opened');
+function showPopup() {
+  popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputOccupation.value = profileOccupation.textContent;
 }
 
-editButton.addEventListener('click', showOrHidePopup);
-closeButton.addEventListener('click', showOrHidePopup);
+function hidePopup() {
+  popup.classList.remove('popup_opened');
+}
 
 function applyInfoChanges(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileOccupation.textContent = inputOccupation.value;
-  showOrHidePopup();
+  hidePopup();
 }
 
+editButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', hidePopup);
 inputForm.addEventListener('submit', applyInfoChanges);
