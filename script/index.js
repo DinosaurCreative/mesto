85ddf12dad7e3311c3bad87a66ff4f4459ff23aa
German Gimalaev <gimalaevgerman@yandex.ru>
2021-03-28 22:43:10 +0300

    Сделать попап с функцией добавления фотографий из массива и по клику

--- script/index.js
+++ script/index.js
@@ -1,76 +1,3 @@
-let page = document.querySelector('.page');
-let closeButtonInfoEdit = page.querySelector('.popup__cross_info-edit');
-let editButton = page.querySelector('.profile__edit-button');
-let profileName = page.querySelector('.profile__name');
-let profileOccupation = page.querySelector('.profile__occupation');
-let inputName = page.querySelector('.popup__input_type_name');
-let inputOccupation = page.querySelector('.popup__input_type_occupation');
-let inputFormEditor = page.querySelector('.popup__form_type_editor');
-let popupInfoEdit = page.querySelector('.popup_info-edit');
-
-function showPopup(className) {
-  className.classList.add('popup_opened');
-}
-
-function addCurrentTextToInput() {
-  inputName.value = profileName.textContent;
-  inputOccupation.value = profileOccupation.textContent;
-}
-
-function hidePopup(className) {
-  className.classList.remove('popup_opened');
-}
-
-function applyInfoChanges() {
-  profileName.textContent = inputName.value;
-  profileOccupation.textContent = inputOccupation.value;
-}
-
-editButton.addEventListener('click', () => {
-  showPopup(popupInfoEdit);
-  addCurrentTextToInput();
-});
-
-
-closeButtonInfoEdit.addEventListener('click', () => {
-  hidePopup(popupInfoEdit);
-});
-
-inputFormEditor.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  applyInfoChanges();
-  hidePopup(popupInfoEdit);
-});
-
-
-// попап ля добавления картинок
-
-// переменные
-const addButton = page.querySelector('.profile__add-button');
-
-const popupImageAdder = page.querySelector('.popup_image-add');
-
-const closeButtonImageAdd = page.querySelector('.popup__cross_image-add');
-
-const inputFormImageAdd = page.querySelector('.popup__form_type_image_add');
-// функции
-
-
-// слушатели  
-addButton.addEventListener('click', () => {
-  showPopup(popupImageAdder);
-});
-
-closeButtonImageAdd.addEventListener('click', () => {
-  hidePopup(popupImageAdder);
-});
-
-inputFormImageAdd.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  // applyInfoChanges();
-  hidePopup(popupImageAdder);
-});
-
 const initialCards = [
   {
     name: 'Эльбрус',
@@ -97,3 +24,179 @@ const initialCards = [
     link: './images/Деревня.jpg'
   }
 ];
+
+const page = document.querySelector('.page');
+const closeButtonInfoEdit = page.querySelector('.popup__cross_info-edit');
+const editButton = page.querySelector('.profile__edit-button');
+const profileName = page.querySelector('.profile__name');
+const profileOccupation = page.querySelector('.profile__occupation');
+const inputName = page.querySelector('.popup__input_type_name');
+const inputOccupation = page.querySelector('.popup__input_type_occupation');
+const inputFormEditor = page.querySelector('.popup__form_type_editor');
+const popupInfoEdit = page.querySelector('.popup_info-edit');
+
+function showPopup(className) {
+  className.classList.add('popup_opened');
+}
+
+function addCurrentTextToInput() {
+  inputName.value = profileName.textContent;
+  inputOccupation.value = profileOccupation.textContent;
+}
+
+function hidePopup(className) {
+  className.classList.remove('popup_opened');
+}
+
+function applyInfoChanges() {
+  profileName.textContent = inputName.value;
+  profileOccupation.textContent = inputOccupation.value;
+}
+
+editButton.addEventListener('click', () => {
+  showPopup(popupInfoEdit);
+  addCurrentTextToInput();
+});
+
+closeButtonInfoEdit.addEventListener('click', () => {
+  hidePopup(popupInfoEdit);
+});
+
+inputFormEditor.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  applyInfoChanges();
+  hidePopup(popupInfoEdit);
+});
+
+
+// Попап добавления изображений
+// переменные
+const addButton = page.querySelector('.profile__add-button');
+const popupImage = page.querySelector('.popup_type_image');
+const closeImagePopup = page.querySelector('.popup__cross_type_image');
+const submitChangesImagePopup = page.querySelector('.popup__form_type_image');
+const gridItemTemplate = page.querySelector('#grid_item').content;
+const gridList = page.querySelector('.grid__list');
+const inputCityTitle = page.querySelector('.popup__input_type_image-title');
+const inputlink = page.querySelector('.popup__input_type_image-link');
+
+
+// Открытие попапа
+addButton.addEventListener('click', () => {
+  showPopup(popupImage);
+})
+
+// Закрытие попапа
+closeImagePopup.addEventListener('click', () => {
+  hidePopup(popupImage);
+});
+
+// Объявление функции добавления новой карточки.
+function addCard(picture, link) {
+  if (link == false) return
+  const gridItem = gridItemTemplate.querySelector('.grid__item').cloneNode(true);
+  gridItem.querySelector('.grid__city-name').textContent = picture;
+  gridItem.querySelector('.grid__image').src = link;
+  gridList.prepend(gridItem);
+}
+// Автодобавления карточек из массива 
+initialCards.forEach(item => {
+  addCard(item.name, item.link);
+})
+
+//  Вызов функции добавления карточки 
+submitChangesImagePopup.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  addCard(inputCityTitle.value, inputlink.value);
+  hidePopup(popupImage);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // блок popup_type_image 
+
+// переменные блока popup_type_image
+// const inputFormImagePopup = page.querySelector('.popup__form_type_image');
+// const inputImageTitle = page.querySelectorAll('.popup__input_type_image-title');
+// const inputImageLink = page.querySelectorAll('.popup__input_type_image-link');
+// функции
+
+
+// // слушатели  
+
+
+// inputFormImagePopup.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   const inputImageTitle = page.querySelectorAll('.popup__input_type_image-title');
+//   const inputImageLink = page.querySelectorAll('.popup__input_type_image-link');
+//   addCard(inputImageLink.value, inputImageTitle.value);
+  
+//   hidePopup(popupImage);
+// });
+
+
+
+
+
+// // Блок добавление грид элемента
+
+// // Объявление переменных
+// const girdList = document.querySelector('.grid__list');  // Div, в который добавляются грид ячейки
+// const gridItemTemplate = document.querySelector('#grid_item').content;   //  шаблон
+// const gridItem = gridItemTemplate.cloneNode(true);  // Занесение шаблона в переменную
+
+// // функция добавления карточки
+
+// function addCard(imageLink, cityName) {
+//   gridItem.querySelector('.grid__image').src = imageLink;
+//   gridItem.querySelector('.grid__city-name').textContent = cityName;
+//   girdList.append(gridItem);
+// }
+
+// // проходчик по массиву
+
+// initialCards.forEach( item => {
+  
+//   gridItemTitle.textContent = item.name;
+//   gridItemImage.src = item.link;
+  
+//   girdLIst.append(gridItem);
+// })
+
+// // слушатели
