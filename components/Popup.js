215fddfd7b62c4c350b93000d196dfcff503a825
German Gimalaev <gimalaevgerman@yandex.ru>
2021-05-12 02:51:09 +0300

    Сверстай предварительную форму класса Popup

--- components/Popup.js
+++ components/Popup.js
@@ -1,12 +1,23 @@
 class Popup {
   constructor(popupSelector) {
-    this._popupSelector = popupSelector;
+    this._popup = document.querySelector(popupSelector);
   }
   open() {
-
+    this._popup.classList.add('popup_opened');
   }
 
   close() {
-    
+    this._popup.classList.remove('popup_opened');
+      
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    };
+  }
+
+  setEventListeners() {
+    document.querySelector('popup__cross').setEventListeners('click', this.close())
   }
 }
